7b55922ace6d6ff9397fb1c83e494949a9d86661
Jacob Ebey <jacob.ebey@live.com>
2024-10-28 23:50:59 -0700

    fix: remove cjs build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/browser.ts", "src/pipeable-dom.ts", "src/jsx.ts"],
 	dts: true,
-	format: ["cjs", "esm"],
+	format: ["esm"],
 	platform: "neutral",
 	external: ["pipeable-dom"],
 });
